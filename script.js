40d1f330a866cd180de6d6833f1c7e18b85830f9
md-akram <akramkaikobad@gmail.com>
2021-08-26 09:32:29 +0600

    completed the project

--- script.js
+++ script.js
@@ -20,8 +20,13 @@ menuHam.addEventListener('click', () => {
 const modalOpener = document.querySelector('#modal-opener')
 const modal = document.querySelector('.modal')
 const modalClose = document.querySelector('#modal-close')
+const getStarted = document.querySelector('#GetStarted')
 
 modalOpener.addEventListener('click', () => {
+  modal.style.display = 'block'
+})
+
+getStarted.addEventListener('click', () => {
   modal.style.display = 'block'
 })
 
@@ -35,6 +40,48 @@ window.onclick = function (event) {
   }
 }
 
+//adding selected class
+
+const radio0 = document.querySelector('#radio')
+const radio1 = document.querySelector('#radio1')
+const radio2 = document.querySelector('#radio2')
+
+const radios = [radio0, radio1, radio2]
+
+radios.forEach((radio) => {
+  radio.addEventListener('click', () => {
+    radios.forEach((r) => {
+      const inputRow = r.parentNode
+      const modalInput = inputRow.parentNode
+      selectedRemover(modalInput)
+    })
+    const inputRow = radio.parentNode
+    const modalInput = inputRow.parentNode
+    selectedAdder(modalInput)
+  })
+})
+
 function selectedAdder(div) {
   div.classList.add('selected')
 }
+function selectedRemover(div) {
+  div.classList.remove('selected')
+}
+
+//success-modal
+
+const continueBtn = document.querySelectorAll('.continue')
+const successModal = document.querySelector('.success-modal')
+
+continueBtn.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    modal.style.display = 'none'
+    successModal.style.display = 'block'
+  })
+})
+
+const gotIt = document.querySelector('.got-it')
+
+gotIt.addEventListener('click', () => {
+  successModal.style.display = 'none'
+})
